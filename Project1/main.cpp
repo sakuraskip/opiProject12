7738729bf0404e4d39ef232d6a8ee87bf936ba02
sakuraskip <144053405+sakuraskip@users.noreply.github.com>
2023-12-29 15:09:10 +0300

    без вопросов

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include <SFML/Graphics.hpp>
 #include "mainmenu.h"
 #include <time.h>
@@ -7,4 +8,15 @@ int main()
 {
     menu();
     return 0;
+=======
+#include <SFML/Graphics.hpp>
+#include "mainmenu.h"
+#include <time.h>
+#include <string>
+
+int main()
+{
+    menu();
+    return 0;
+>>>>>>> 66cad6d34c7bf69efa7783a7f1b424ac486609e0
 }
--- Project1/mainmenu.h
+++ Project1/mainmenu.h
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #pragma once
 
 int menu();
@@ -10,3 +11,17 @@ int main();
 
 int game();
 
+=======
+#pragma once
+
+int menu();
+
+int authorspagee();
+
+int moreAbout();
+
+int main();
+
+int game();
+
+>>>>>>> 66cad6d34c7bf69efa7783a7f1b424ac486609e0
